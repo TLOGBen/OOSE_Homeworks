7b94418f449811d08ba52e17189e130ae76a0113
Ben Tsai <powdersave@gmail.com>
2015-12-21 17:01:52 +0800

    Finish

--- Homework/src/Main.java
+++ Homework/src/Main.java
@@ -3,7 +3,11 @@
  */
 public class Main{
     public static void main(String[] argv){
-
+        Quesation q1 = new Quesation("xxx");
+        Quesation q2 = new Quesation("yyy");
+        QuesationAdd qa = new QuesationAdd();
+        qa.AddQuestion(q1);
+        qa.AddQuestion(q2);
     }
 }
 class Quesation{
@@ -13,5 +17,21 @@ class Quesation{
     }
     public void print(){
         System.out.println(discription);
+    }
+}
+class QuesationAdd extends Quesation{
+    Arraylist<Quesation> list;
+    public QuesationAdd(){
+        list = new ArrayList<Quesation>();
+    }
+    public void AddQuestion(Quesation q){
+        list.add(q);
+    }
+    public void op(){
+        ListerIterator<Quesation> iterator = list.listIterator();
+        while (iterator.hasNext()){
+            Quesation q = iterator.next();
+            q.print();
+        }
     }
 }
